a2207c2421ccb3093fb64802c23afa827fa50ea2
Bouclet Emeline <mimi@MacBook-Air-de-Bouclet.local>
2023-08-06 14:16:58 +0200

    correction fail build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Comics from "./pages/comics";
+import Comics from "./pages/Comics";
 import Characters from "./pages/characters";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
